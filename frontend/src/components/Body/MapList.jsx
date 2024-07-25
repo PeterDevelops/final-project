727516781f670f15fee281ed0029e635f5f5154b
Lisa Wild <lisakwild@gmail.com>
2024-07-25 11:53:34 -0700

    chore: add component folders and initial component files

---
+++ frontend/src/components/Body/MapList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MapList = () => {
+  return (
+    <div></div>
+
+  )
+
+};
+
+export default MapList;
---
+++ frontend/src/components/NavBar Components/HamburgerMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HamburgerMenu = () => {
+  return (
+    <div>
+      
+    </div>
+
+  )
+
+};
+
+export default HamburgerMenu;
