7eb67eff8024cc89914c2cdb18c12d9e7b86b9c6
Lisa Wild <lisakwild@gmail.com>
2024-07-29 11:01:23 -0700

    chore: add vendor query and routing for backend, standardize photo size for products and vendors

--- backend/db/seeds/02_vendors.sql
+++ backend/db/seeds/02_vendors.sql
@@ -1,29 +1,29 @@
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (1, 'Torp, McDermott and Cassin', 'sit amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque', '409 Donald Center', 'Port Moody, BC', -122.8677562, 49.2849107, 'http://dummyimage.com/362x277.png/cc0000/ffffff', 1);
+VALUES (1, 'Torp, McDermott and Cassin', 'sit amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum ac tellus semper interdum mauris ullamcorper purus sit amet nulla quisque', '409 Donald Center', 'Port Moody, BC', -122.8677562, 49.2849107, 'http://dummyimage.com/369x323.png/dddddd/000000', 1);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
 VALUES (2, 'Mraz-Wintheiser', 'justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante vivamus tortor duis mattis egestas metus aenean fermentum donec', '15 Petterle Drive', 'Chilliwack, BC', -121.951469, 49.157940, 'http://dummyimage.com/369x323.png/dddddd/000000', 2);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (3, 'Blanda, Fahey and Konopelski', 'suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed', '54166 Hanover Alley', 'Nanaimo, BC', -123.940063, 49.165882, 'http://dummyimage.com/119x144.png/5fa2dd/ffffff', 3);
+VALUES (3, 'Blanda, Fahey and Konopelski', 'suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed', '54166 Hanover Alley', 'Nanaimo, BC', -123.940063, 49.165882, 'http://dummyimage.com/369x323.png/5fa2dd/ffffff', 3);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (4, 'Ullrich-Collins', 'felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis', '1 Gale Street', 'Chilliwack, BC', -121.951469, 49.057940, 'http://dummyimage.com/385x339.png/dddddd/000000', 4);
+VALUES (4, 'Ullrich-Collins', 'felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis', '1 Gale Street', 'Chilliwack, BC', -121.951469, 49.057940, 'http://dummyimage.com/369x323.png/dddddd/000000', 4);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (5, 'Simonis Inc', 'in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent', '1283 Moland Junction', 'Port Moody, BC', -122.8677562, 49.2649107, 'http://dummyimage.com/111x196.png/cc0000/ffffff', 5);
+VALUES (5, 'Simonis Inc', 'in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent', '1283 Moland Junction', 'Port Moody, BC', -122.8677562, 49.2649107, 'http://dummyimage.com/369x323.png/cc0000/ffffff', 5);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (6, 'Kulas Inc', 'penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida sem praesent id', '675 Scoville Avenue', 'Nanaimo, BC', -123.940063, 49.065882, 'http://dummyimage.com/146x257.png/ff4444/ffffff', 6);
+VALUES (6, 'Kulas Inc', 'penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida sem praesent id', '675 Scoville Avenue', 'Nanaimo, BC', -123.940063, 49.065882, 'http://dummyimage.com/369x323.png/ff4444/ffffff', 6);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (7, 'Nitzsche Inc', 'dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi nam ultrices libero non mattis pulvinar nulla', '11232 Jenna Point', 'Nanaimo, BC', -123.940063, 49.265882, 'http://dummyimage.com/152x366.png/5fa2dd/ffffff', 7);
+VALUES (7, 'Nitzsche Inc', 'dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi nam ultrices libero non mattis pulvinar nulla', '11232 Jenna Point', 'Nanaimo, BC', -123.940063, 49.265882, 'http://dummyimage.com/369x323.png/5fa2dd/ffffff', 7);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (8, 'Legros-Rolfson', 'curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam', '570 Arizona Alley', 'Campbell River, BC', -125.2418, 50.0213, 'http://dummyimage.com/173x190.png/dddddd/000000', 8);
+VALUES (8, 'Legros-Rolfson', 'curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam', '570 Arizona Alley', 'Campbell River, BC', -125.2418, 50.0213, 'http://dummyimage.com/369x323.png/dddddd/000000', 8);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (9, 'Hettinger, Effertz and Bergstrom', 'sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus', '940 Washington Point', 'Campbell River, BC', -125.2418, 50.0213, 'http://dummyimage.com/100x361.png/5fa2dd/ffffff', 9);
+VALUES (9, 'Hettinger, Effertz and Bergstrom', 'sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus', '940 Washington Point', 'Campbell River, BC', -125.2418, 50.0213, 'http://dummyimage.com/369x323.png/5fa2dd/ffffff', 9);
 
 INSERT INTO vendors (id, name, bio, address, city, longitude, latitude, vendor_logo_url, admin_user) 
-VALUES (10, 'O''Conner, Kilback and Powlowski', 'turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt', '20892 Dennis Lane', 'Chilliwack, BC', -121.951469, 49.157940, 'http://dummyimage.com/131x328.png/cc0000/ffffff', 10);
+VALUES (10, 'O''Conner, Kilback and Powlowski', 'turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt', '20892 Dennis Lane', 'Chilliwack, BC', -121.951469, 49.157940, 'http://dummyimage.com/369x323.png/cc0000/ffffff', 10);
