1aeefa9fc82ec6a9bfe920c7ad1ab82f2b397d7a
PeterDevelops <PeterDinhBC@gmail.com>
2024-07-30 14:39:30 -0700

    add checkout component and delivery toggle component

--- frontend/src/components/Routes/Checkout.jsx
+++ frontend/src/components/Routes/Checkout.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import NavBar from '../NavBar';
 import '../../styles/Cart.scss';
-import { Link } from 'react-router-dom';
+import DeliveryToggle from '../DeliveryToggle';
 
-const Cart = ({ products, vendors, locations }) => {
+const Checkout = ({ products, vendors, locations }) => {
 
   const navigate = useNavigate();
 
@@ -13,35 +13,34 @@ const Cart = ({ products, vendors, locations }) => {
     <div>
       <NavBar products={products} vendors={vendors} locations={locations} />
 
+
       <div className='cart-container'>
+
         <div className='cart-center'>
-          <span>Vendor Photo</span>
-          <span className='span-tag'>Vendor Name</span>
+          <DeliveryToggle />
         </div>
 
         <div className='cart-center'>
-          Your Cart
+          Order Summary
         </div>
 
         <div className='cart-center'>
           <span>Cart Item Photo</span>
           <span className='span-tag'>Cart Item Info</span>
-          <span className='span-tag'>Cart Item Subtotal</span>
+          
         </div>
 
         <div className='total'>
           Total: $$$
         </div>
-        <Link to='/checkout'>
-          <div className='cart-center'>
-            <button className='text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-300
+
+        <div className='cart-center'>
+          <button className='text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-300
         font-medium rounded-full text-sm px-5 py-2 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700
         dark:focus:ring-green-800'>
-              Checkout
-            </button>
-          </div>
-        </Link>
-
+            Pay
+          </button>
+        </div>
         <div className='cart-center'>
           <button onClick={() => navigate('/')}>Continue Shopping</button>
         </div>
@@ -51,4 +50,4 @@ const Cart = ({ products, vendors, locations }) => {
   );
 };
 
-export default Cart;
+export default Checkout;
