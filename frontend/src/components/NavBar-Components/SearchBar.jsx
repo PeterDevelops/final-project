735c49bb05ba496a3c625723131b8b828c5b2171
Lisa Wild <lisakwild@gmail.com>
2024-07-30 09:47:34 -0700

    feat: add autocomplete filterOption to search bar to control the number of suggestions

--- frontend/src/components/NavBar-Components/SearchBar.jsx
+++ frontend/src/components/NavBar-Components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { React, useState } from 'react';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSeedling, faStore, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -77,6 +77,12 @@ export default function Grouped(props) {
 
   // console.log('combined data-------', combinedData());
 
+  //filter the NUMBER of suggestions that show (how to show 2 in each category)
+  const filterOptions = createFilterOptions({
+    limit: 2,
+   
+  });
+
   return (
     <div className="p-4 max-w-md mx-auto">
       <Autocomplete
@@ -85,7 +91,9 @@ export default function Grouped(props) {
         groupBy={(option) => option.category}
         getOptionLabel={(option) => option.name}
         sx={{ width: '100%' }}
+        filterOptions={filterOptions}
         renderOption={({ props }, option) => (
+          
           <li {...props} key={option.id} className="flex items-center p-2 border border-gray-300 rounded-md cursor-pointer">
             <FontAwesomeIcon icon={option.icon} className="mr-2" />
             <span>{option.name}</span>
@@ -107,6 +115,7 @@ export default function Grouped(props) {
             }}
           />
         )}
+        
       />
     </div>
   );
