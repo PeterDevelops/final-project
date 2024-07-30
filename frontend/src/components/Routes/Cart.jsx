135c13779c087cc6dc3b9e6101c65f7426719122
PeterDevelops <PeterDinhBC@gmail.com>
2024-07-30 12:57:21 -0700

    add css

--- frontend/src/components/Routes/Cart.jsx
+++ frontend/src/components/Routes/Cart.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate, useLocation } from 'react-router-dom';
-import HamburgerMenu from '../NavBar-Components/HamburgerMenu';
-import { Link } from 'react-router-dom';
 import NavBar from '../NavBar';
+import '../../styles/Cart.scss';
 
 const Cart = ({ products, vendors, locations }) => {
 
@@ -16,29 +12,37 @@ const Cart = ({ products, vendors, locations }) => {
     <div>
       <NavBar products={products} vendors={vendors} locations={locations} />
 
-      <div>
+    <div className='cart-container'>
+      <div className='cart-center'>
         <span>Vendor Photo</span>
-        <span>Vendor Name</span>
+        <span className='span-tag'>Vendor Name</span>
       </div>
 
-      <div>
+      <div className='cart-center'>
         Your Cart
       </div>
 
-      <div>
+        <div className='cart-center'>
+        <span>Cart Item Photo</span>
+        <span className='span-tag'>Cart Item Info</span>
+        <span className='span-tag'>Cart Item Subtotal</span>
+        </div>
+
+      <div className='total'>
         Total: $$$
       </div>
 
-      <div>
+      <div className='cart-center'>
         <button className='text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-green-300
         font-medium rounded-full text-sm px-5 py-2 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700
         dark:focus:ring-green-800'>
           Checkout
         </button>
       </div>
+      <div className='cart-center'>
+        <button onClick={() => navigate('/')}>Continue Shopping</button>
+      </div>
 
-      <div>
-        <button onClick={() => navigate('/')}>Go to home page</button>
       </div>
     </div>
   );
