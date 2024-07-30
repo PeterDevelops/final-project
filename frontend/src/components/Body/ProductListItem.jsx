6093e5047de8aadc5704d0d5139b1eb7e48443ca
Jdob11 <jbdobson11@gmail.com>
2024-07-30 15:34:45 -0700

    style: Fix style so images/logos are not rounded, but don't overflow the container

--- frontend/src/components/Body/ProductListItem.jsx
+++ frontend/src/components/Body/ProductListItem.jsx
@@ -16,11 +16,11 @@ const ProductListItem = (props) => {
   const { productData } = props;
 
   return (
-    <article className="flex flex-col md:flex-row md:items-center border rounded-lg shadow-md m-5">
+    <article className="flex flex-col md:flex-row md:items-center border rounded-lg shadow-md m-5 overflow-hidden">
       <img
         src={productData.photo_url}
         alt={`${productData.name} image`}
-        className="w-full md:w-1/3 rounded-lg md:rounded-none object-cover md:object-contain"
+        className="w-full md:w-1/3 md:object-cover md:object-contain"
       />
       <div className="p-5 w-full md:w-2/3">
         <h1 className="text-xl font-semibold">{productData.name}</h1>
--- frontend/src/components/Body/VendorListItem.jsx
+++ frontend/src/components/Body/VendorListItem.jsx
@@ -4,11 +4,11 @@ const VendorListItem = (props) => {
   const { vendorData } = props;
 
   return (
-    <article className="flex flex-col md:flex-row md:items-center border rounded-lg shadow-md m-5">
+    <article className="flex flex-col md:flex-row md:items-center border rounded-lg shadow-md m-5 overflow-hidden">
       <img
         src={vendorData.vendor_logo_url}
         alt="vendor logo"
-        className="w-full md:w-1/3 md:rounded-none object-cover md:object-contain"
+        className="w-full md:w-1/3 md:object-cover md:object-contain"
       />
       <div className="p-5 w-full md:w-2/3">
         <h3 className="text-xl font-semibold">{vendorData.name}</h3>
