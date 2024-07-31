bbd2ff5f297e6b219025229b757852aa40151ac8
Lisa Wild <lisakwild@gmail.com>
2024-07-31 15:20:31 -0700

    chore: standarized login credentials for development purposes

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,14 +1,14 @@
 INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status) 
-VALUES ('Peter', 'peter@peter.com', '$2a$04$9k1D3e8XS0MyU5hbt9NYH./mzFAidshiQQEoNm5kWlztTJQgaMpoK', 'http://dummyimage.com/174x166.png/5fa2dd/ffffff', true);
+VALUES ('Peter', 'peter@peter.com', '1234', 'http://dummyimage.com/174x166.png/5fa2dd/ffffff', true);
 
 INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status) 
-VALUES ('Lily', 'lily@lily.com', '$2a$04$mLqRXXkrI.ho9c5ZVYQzYuGjn84TJaP8ziVCjQ8eCJrEo8SaSwgMy', 'http://dummyimage.com/198x163.png/ff4444/ffffff', true);
+VALUES ('Lily', 'lily@lily.com', '1234', 'http://dummyimage.com/198x163.png/ff4444/ffffff', true);
 
 INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status) 
-VALUES ('Jeff', 'jeff@jeff.com', '$2a$04$11xfuGjCGUJrnvGpZDEKZ.l3LfzfUfV9bfjeg6hQjHbtu1zlPnxuy', 'http://dummyimage.com/179x179.png/ff4444/ffffff', true);
+VALUES ('Jeff', 'jeff@jeff.com', '1234', 'http://dummyimage.com/179x179.png/ff4444/ffffff', true);
 
 INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status) 
-VALUES ('Lisa', 'lisa@lisa.com', '$2a$04$pGsffXrkyjxnuiPYk0tabuVWl.3RpbKszGAWfwETAXBBrIj/c3SJ.', 'http://dummyimage.com/205x228.png/ff4444/ffffff', true);
+VALUES ('Lisa', 'lisa@lisa.com', '1234', 'http://dummyimage.com/205x228.png/ff4444/ffffff', true);
 
 INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status) 
 VALUES ('Melisse', 'mgerrelt0@newyorker.com', '$2a$04$MlOtXAhYNHnP2x4USjTapOhOR95JscetzB9t55Cz9fWLzgl3UE20y', 'http://dummyimage.com/158x100.png/ff4444/ffffff', true);
@@ -29,4 +29,4 @@ INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status
 VALUES ('Bob', 'bob@bob.com', '$2a$04$rZy8/2ffyS1goMg8iMPv2uWkQpNGit11Qgl8p/D5Mf1rhEYZR.KGq', 'http://dummyimage.com/246x100.png/dddddd/000000', true);
 
 INSERT INTO users (name, email, hashed_password, profile_photo_url, admin_status) 
-VALUES ('Guest', 'guest@rguest.com', '$2a$04$l5PZ6IWPcGdkIaJ1v5NpquXxW7BYSCSp9a8iTJghAfWhuY2N3vwiG', 'http://dummyimage.com/210x100.png/5fa2dd/ffffff', false);
+VALUES ('Guest', 'guest@rguest.com', '1234', 'http://dummyimage.com/210x100.png/5fa2dd/ffffff', false);
