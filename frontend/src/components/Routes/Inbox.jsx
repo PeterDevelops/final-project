b0d120748eb9785f9d197d1eeda757296570c1e8
Lisa Wild <lisakwild@gmail.com>
2024-08-03 19:40:52 -0700

    bugfix: pull axios request for inbox into the inbox component to fix data delay

--- frontend/src/components/Routes/Inbox.jsx
+++ frontend/src/components/Routes/Inbox.jsx
@@ -1,43 +1,54 @@
 import React from 'react';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import NavBar from '../NavBar';
 import ChatList from '../Body/ChatList'
 import io from 'socket.io-client'
 const socket = io.connect("http://localhost:8080")
 
 const Inbox = ({ products, vendors, locations, categories, user, setUser}) => {
-  const location = useLocation();
   const navigate = useNavigate();
-  const { inboxData } = location.state;
 
   const [username, setUsername] = useState("");
+  const [chatData,setChatData] = useState([]);
   const [chat, setChat] = useState('');
-
-  // console.log("INBOX DATA----", inboxData)
-
-
+ 
   useEffect(() => {
     if (user) {
-      // console.log("user-----", user)
-      setUsername(user.name)
+      axios.get(`/api/chats/${user.id}`)
+        .then(response => {
+          const userChatData = response.data;
+          console.log("USER CHAT DATA------", userChatData)
+          console.log("USER CHAT DATA LENGTH------", userChatData.length)
+          setChatData(userChatData);
+          setUsername(user.name);
+        })
+        .catch(error => {
+          console.error('Error retrieving inbox data:', error);
+        });
+    } else {
+      console.log('Not logged in');
     }
-  }, [])
-
-
+  }, [user]);
+  
+  
   // const joinChat = (chatId) => {
     // console.log("Chat clicked");
     // if (username !== "" && chat !== "") {
-    //   socket.emit("join_chat", {chatId})
-
-    // }
-  // }
-
+      //   socket.emit("join_chat", {chatId})
+      
+      // }
+      // }
+  
+  console.log("CHAT DATA IN INBOX----", chatData)
+      
   const chatListArr = () => {
-    if (Array.isArray(inboxData) && inboxData.length > 0) {
-      const chatList = inboxData.map((chat) => <ChatList key={chat.id} chat={chat} user={user}/>)
+    if (Array.isArray(chatData) && chatData.length > 0) {
+      console.log("ChatData exists in the inbox")
+      const chatList = chatData.map((chat) => <ChatList key={chat.id} chat={chat} user={user}/>)
       return chatList;
     }
   }
@@ -46,7 +57,24 @@ const Inbox = ({ products, vendors, locations, categories, user, setUser}) => {
     <div>
       <NavBar products={products} vendors={vendors} locations={locations} categories={categories} user={user} setUser={setUser}/>
       <h1 className="text-xl font-semibold m-3 p-3">Inbox </h1>
-      {chatListArr()}
+      
+
+      <div className="flex flex-col justify-content">
+      <div className="bg-[#F7F4F0] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="bg-[#F7F4F0] rounded p-2">
+        {chatListArr()}
+        </div>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
     </div>
   );
 };
