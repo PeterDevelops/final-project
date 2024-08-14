f15367ffdc42d6b8182b88735c535b9929f89b2f
Jdob11 <jbdobson11@gmail.com>
2024-08-14 12:28:21 -0700

    chore: Clean up formatting on all components in NavBar folder

--- frontend/src/components/NavBar-Components/SearchBar.jsx
+++ frontend/src/components/NavBar-Components/SearchBar.jsx
@@ -11,12 +11,12 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
-export default function SearchBar(props) {
+const SearchBar = (props) => {
   const {
     setProducts,
     allProducts,
     setVendors,
-    allVendors,
+    allVendors
   } = props;
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState('');
@@ -24,79 +24,64 @@ export default function SearchBar(props) {
   const inputRef = useRef(null);
 
   const categorizeProducts = () => {
-    if (Array.isArray(allProducts) && allProducts.length > 0) {
-      const subCategories = {};
+    if (!Array.isArray(allProducts) || !allProducts.length) return [];
 
-      allProducts.forEach(product => {
-        if (product.sub_category) {
-          const id = `${product.category}-${product.sub_category}`;
-          if (!subCategories[id]) {
-            subCategories[id] = {
-              id,
-              category: product.category,
-              name: product.sub_category,
-              icon: getIconForCategory(product.category),
-            };
-          }
+    const subCategories = {};
+
+    allProducts.forEach(product => {
+      if (product.sub_category) {
+        const id = `${product.category}-${product.sub_category}`;
+        if (!subCategories[id]) {
+          subCategories[id] = {
+            id,
+            category: product.category,
+            name: product.sub_category,
+            icon: getIconForCategory(product.category),
+          };
         }
-      });
-      return Object.values(subCategories);
-    }
-    return [];
+      }
+    });
+
+    return Object.values(subCategories);
   };
 
-
   const categorizeVendors = () => {
-    if (Array.isArray(allVendors) && allVendors.length > 0) {
-      return allVendors.map((vendor) => ({
-        ...vendor,
-        key: vendor.id,
-        category: 'Vendor',
-        icon: getIconForCategory('Vendor'),
-      }));
-    }
-    return [];
+    if (!Array.isArray(allVendors) || !allVendors.length) return [];
+
+    return allVendors.map(vendor => ({
+      ...vendor,
+      key: vendor.id,
+      category: 'Vendor',
+      icon: getIconForCategory('Vendor'),
+    }));
   };
 
   const combinedData = () => {
     const productData = categorizeProducts();
     const vendorData = categorizeVendors();
 
-    const allData = [...productData, ...vendorData];
-
-    return allData.sort((a, b) => a.category.localeCompare(b.category));
+    return [...productData, ...vendorData].sort((a, b) => a.category.localeCompare(b.category));
   };
 
-  function getIconForCategory(category) {
+  const getIconForCategory = (category) => {
     switch (category) {
-      case 'Vegetable':
-        return faSeedling;
-      case 'Fruit':
-        return faAppleWhole;
-      case 'Meat':
-        return faDrumstickBite;
-      case 'Miscellaneous':
-        return faBreadSlice;
-      case 'Vendor':
-        return faStore;
-      default:
-        return null;
+      case 'Vegetable': return faSeedling;
+      case 'Fruit': return faAppleWhole;
+      case 'Meat': return faDrumstickBite;
+      case 'Miscellaneous': return faBreadSlice;
+      case 'Vendor': return faStore;
+      default: return null;
     }
-  }
+  };
 
   const handleOptionClick = (option) => {
-    if (inputRef.current) {
-      inputRef.current.blur();
-    }
+    if (inputRef.current) inputRef.current.blur();
 
     if (option.vendor_logo_url) {
       const filteredByVendor = allProducts.filter(product => product.vendor_id === option.id);
-      const currentVendor = [option]
-      console.log('Current Vendor: ', currentVendor);
-
       setProducts(filteredByVendor);
-      setVendors(currentVendor)
-      navigate(`/vendors/:${currentVendor[0].id}`, { state: { allProducts, allVendors } });
+      setVendors([option]);
+      navigate(`/vendors/${option.id}`, { state: { allProducts, allVendors } });
     } else {
       const filteredBySubCategory = allProducts.filter(product => product.sub_category === option.name);
       setProducts(filteredBySubCategory);
@@ -110,22 +95,16 @@ export default function SearchBar(props) {
     const matchedVendor = allVendors.find(vendor => vendor.name.toLowerCase() === inputValue.toLowerCase());
     const matchedSubCategory = allProducts.find(product => product.sub_category.toLowerCase() === inputValue.toLowerCase());
 
-    if (inputRef.current) {
-      inputRef.current.blur();
-    }
+    if (inputRef.current) inputRef.current.blur();
 
     if (matchedVendor) {
       const filteredByVendor = allProducts.filter(product => product.vendor_id === matchedVendor.id);
       setProducts(filteredByVendor);
-      setVendors(matchedVendor)
-      setInputValue('');
-      setFilteredOptions([]);
-      navigate(`/vendors/:${matchedVendor.id}`, { state: { allProducts, allVendors } });
+      setVendors([matchedVendor]);
+      navigate(`/vendors/${matchedVendor.id}`, { state: { allProducts, allVendors } });
     } else if (matchedSubCategory) {
       const filteredBySubCategory = allProducts.filter(product => product.sub_category.toLowerCase() === inputValue.toLowerCase());
       setProducts(filteredBySubCategory);
-      setInputValue('');
-      setFilteredOptions([]);
       navigate('/products', { state: { allProducts } });
     } else if (filteredOptions.length > 0) {
       handleOptionClick(filteredOptions[0]);
@@ -144,17 +123,17 @@ export default function SearchBar(props) {
     <div className='p-4 max-w-xl mx-auto font-body'>
       <form onSubmit={handleSubmit} className='font-body'>
         <Autocomplete
-          id='grouped-demo'
+          id='search-bar'
           options={combinedData()}
-          groupBy={(option) => option.category}
-          getOptionLabel={(option) => option.name}
+          groupBy={option => option.category}
+          getOptionLabel={option => option.name}
           inputValue={inputValue}
           onInputChange={handleInputChange}
           sx={{ width: '100%' }}
-          renderOption={({ props }, option) => (
+          renderOption={(props, option) => (
             <li
               {...props}
-              key={option.id}
+              key={option.key}
               className='flex items-center p-2 rounded-md cursor-pointer font-body'
               onClick={() => handleOptionClick(option)}
             >
@@ -166,7 +145,7 @@ export default function SearchBar(props) {
             <TextField
               {...params}
               label='Search by Product Type or Vendor'
-              className='bg-gray-100 border-4 border-border shadow-md font-body border-4 border-border'
+              className='bg-gray-100 border-4 border-border shadow-md font-body'
               sx={{
                 borderRadius: '9999px',
                 '& .MuiOutlinedInput-root': {
@@ -174,11 +153,11 @@ export default function SearchBar(props) {
                   border: '2px solid #564225',
                 },
                 '& .MuiInputLabel-root.Mui-focused': {
-                  color: '#564225',
+                  color: '#e2e7de',
                   transform: 'translate(16px, -20px) scale(0.75)',
                 },
                 '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                  borderColor: '#4b5a3d',
+                  border:'none',
                 },
               }}
               inputRef={inputRef}
@@ -188,4 +167,6 @@ export default function SearchBar(props) {
       </form>
     </div>
   );
-}
+};
+
+export default SearchBar;
